d0497fc3af0ae3eece6160be02130a0b97598fd5
seyedmohammadreza <59357443+mrakharatha@users.noreply.github.com>
2023-10-27 15:36:05 +0330

    Making the appearance of the application

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,6 +8,21 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	
+
+	private void Sld_OnValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		throw new NotImplementedException();
+	}
+
+
+	private void ImageButton_OnClicked(object sender, EventArgs e)
+	{
+		throw new NotImplementedException();
+	}
+
+	private void BtnRandom_OnClicked(object sender, EventArgs e)
+	{
+		throw new NotImplementedException();
+	}
 }
 
